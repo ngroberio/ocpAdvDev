d6cc53275eaebf05ee086a9f2d648000c87c093b
Nilton Groberio <ngroberio@gmail.com>
2018-08-06 16:51:44 +0100

    + Add sonar setup script

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -7,10 +7,17 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Setting up Sonarqube in project $GUID-sonarqube"
+echo ">> Setting up Sonarqube in project $GUID-sonarqube"
 
 # Code to set up the SonarQube project.
 # Ideally just calls a template
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+TMPL=./Infrastructure/templates/sonar.yaml
+PROJ=$GUID-sonarqube
+# PROJ=ngroberio-test
+oc create -f $TMPL -n $PROJ
+
+echo ">> Sonarqube liveness check"
+./Infrastructure/bin/waitPodReady.sh sonarqube ${PROJ}

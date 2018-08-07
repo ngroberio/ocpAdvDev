8c58192ec0f5e2f967c5ba8299ab2d5f215613a7
Nilton Groberio <ngroberio@gmail.com>
2018-08-07 11:49:16 +0100

    +Updating liveness check for 7 seconds

--- Infrastructure/bin/podLivenessCheck.sh
+++ Infrastructure/bin/podLivenessCheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo ">> Liveness Check for Pod ${1} from Project ${2}"
-sleep 5
+sleep 7
 while : ; do
   echo ">> Check Pod ${1} is Ready..."
   oc get pod -n $2 | grep $1 | grep -v build | grep -v deploy |grep "1/1.*Running"
   [[ "$?" == "1" ]] || break
-  echo "...no. Sleeping 15 seconds."
-  sleep 15
+  echo "...no. Sleeping 7 seconds."
+  sleep 7
 done

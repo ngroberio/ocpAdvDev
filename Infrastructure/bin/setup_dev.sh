eb85700238d2d852e781a1dca9b7b456cbab6378
Nilton Groberio <ngroberio@gmail.com>
2018-08-07 12:04:51 +0100

    +Update script

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,3 +12,17 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
+PROJ=${GUID}-parks-dev
+MONGO_TMPL=./Infrastructure/templates/mongo.yaml
+APPS_TMPL=./Infrastructure/templates/dev_apps.yaml
+
+echo ">>> STEP #1 > CREATE MONGODB"
+oc create -f $MONGO_TMPL -n $PROJ
+
+./Infrastructure/bin/podLivenessCheck.sh mongodb $PROJ
+
+echo ">>> STEP #2 > CREATE APPS"
+oc create -f $APPS_TMPL -n $PROJ
+
+echo ">>> STEP #3 > ADD PERMISSIONS"
+oc policy add-role-to-user view --serviceaccount=default -n $PROJ

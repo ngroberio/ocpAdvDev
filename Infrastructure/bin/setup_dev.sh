4d564fffd7e7bce5cd25df44891b9c8afccd8a10
Nilton Groberio <ngroberio@gmail.com>
2018-08-10 22:36:49 +0100

    + Update dev stup deployment configs

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -14,7 +14,10 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # To be Implemented by Student
 PROJECT_NAME=${GUID}-parks-dev
 MONGO_TMPL=./Infrastructure/templates/mongodb_template.yaml
-APPS_TMPL=./Infrastructure/templates/dev_setup_template.yaml
+MLB_TMPL=./Infrastructure/templates/dev_setup_mlbparks_template.yaml
+NATIONAL_TMPL=./Infrastructure/templates/dev_setup_nationalparks_template.yaml
+PARKS_TMPL=./Infrastructure/templates/dev_setup_parksmap_template.yaml
+
 
 echo ">>> STEP #1 > CREATE MONGODB"
 oc create -f $MONGO_TMPL -n $PROJECT_NAME
@@ -22,7 +25,14 @@ oc create -f $MONGO_TMPL -n $PROJECT_NAME
 ./Infrastructure/bin/podLivenessCheck.sh mongodb $PROJECT_NAME
 
 echo ">>> STEP #2 > CREATE APPS"
-oc create -f $APPS_TMPL -n $PROJECT_NAME
+oc create -f $MLB_TMPL -n $PROJECT_NAME
+sleep 10
+
+oc create -f $NATIONAL_TMPL -n $PROJECT_NAME
+sleep 10
+
+oc create -f $PARKS_TMPL -n $PROJECT_NAME
+sleep 10
 
 echo ">>> STEP #3 > ADD PERMISSIONS"
 oc policy add-role-to-user view --serviceaccount=default -n $PROJECT_NAME
